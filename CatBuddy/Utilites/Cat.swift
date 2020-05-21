334c0953f9ee2cefc66d78a35031514d42af75bb
Maksym Sabadyshyn <max.sabadyshyn@gmail.com>
2020-05-21 19:27:32 +0300

    init commit. gitignore added, Cat model init template added

---
+++ CatBuddy/Utilites/Cat.swift
@@ -0,0 +1,17 @@
+//
+//  Cat.swift
+//  CatBuddy
+//
+//  Created by Maksym Sabadyshyn on 5/21/20.
+//  Copyright © 2020 Maksym Sabadyshyn. All rights reserved.
+//
+
+import Foundation
+
+struct Cat: Decodable {
+    
+}
+
+struct Breed: Decodable {
+    
+}
